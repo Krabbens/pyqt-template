1a95e5d1c5958c1bde9aa6792d4703974025e24a
Krabbens <50266271+Krabbens@users.noreply.github.com>
2022-07-23 16:48:48 +0200

    Remove not used imports

--- Callback.py
+++ Callback.py
@@ -1,5 +1,3 @@
-from Models.HintResultList import HintResultList
-from Models.AlbumsResultList import AlbumsResultList
 from Connector import Connector
 from PyQt5.QtCore import QCoreApplication, QUrl, pyqtSignal, QObject
 from PyQt5.QtCore import pyqtSlot
@@ -7,19 +5,13 @@ from PyQt5.QtQml import QQmlApplicationEngine
 from PyQt5.QtQml import qmlRegisterType, QQmlComponent
 from Thread import Thread
 from concurrent.futures import ThreadPoolExecutor as cf
-from Models.PersonResultList import PersonResultList
-from Models.Image import Image
 import os
-import ctypes
-import json
 
 class Callback(QObject):
     def __init__(self, program):
         QObject.__init__(self)
         self.program = program
         self.engine = None
-        self.personList = PersonResultList()
-        self.background = Image(self.program.fullPath)
         self.connector = Connector(self.program)
         self.threads = []
         self.threadId = 0
@@ -34,8 +26,3 @@ class Callback(QObject):
     @pyqtSlot()
     def Exit(self):
         os._exit(0)
-
-    @pyqtSlot()
-    def Minimize(self):
-        poly_window = ctypes.windll.user32.FindWindowW(None, "polyflow")
-        ctypes.windll.user32.ShowWindow(poly_window, 6)
--- Connector.py
+++ Connector.py
@@ -1,15 +1,9 @@
 from PyQt5 import QtCore, QtWidgets, QtQml
 
 class Connector(QtCore.QObject):
-    thumbnailSignal = QtCore.pyqtSignal(name='thumbnailReload')
-    updateProgressSignal = QtCore.pyqtSignal(QtCore.QPointF, name='updateProgess')
-
     def __init__(self, program, parent=None):
         super(Connector, self).__init__(parent)
         self.program = program
 
     def Init(self):
         self.root = self.program.engine.rootObjects()[0]
-
-    # def thumbnailReload(self):
-    #     self.thumbnailSignal.emit()
